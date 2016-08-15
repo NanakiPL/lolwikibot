9cd8ae277d26c00ae103966e7074f41f8c058a81
Nanaki <nanaki@wikia-inc.com>
2016-08-15 10:53:03 +0200

    Zero-state config files

---
+++ user-config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8  -*-
+from __future__ import unicode_literals
+
+family = 'lol'
+mylang = 'en'
+usernames = { family: {} }
+sysopnames = { family: {} }
+disambiguation_comment = { family: {} }
+family_files = { family: 'family.py' }
+
+# Enter your usernames for langs where you have a flag on. Keep the above part as is
+
+usernames['lol']['en'] = u'Username'
+#usernames['lol']['de'] = u'Username'
+#usernames['lol']['fr'] = u'Username'
+#usernames['lol']['pl'] = u'Username'
