37efac36044add801601fc3065a0aca063639665
Nanaki <nanaki@wikia-inc.com>
2016-08-23 19:23:52 +0200

    first run config handling

--- lol.py
+++ lol.py
@@ -1,32 +1,37 @@
 # -*- coding: utf-8  -*-
+import os
+
+config_file = 'user-config.py'
 
 def checkUserConfig():
-    try:
-        open('user-configs.py').close()
-    except IOError as e:
-        if e.errno == 2:
-            import config
-            return config.main()
-        raise
-        
+    global config_file
+    if not os.path.exists(config_file):
+        import config
+        config.main(True)
+        return False
+    return True
     
 def main():
-    family = checkUserConfig()
-    
-    print('fam %s' % family)
-    import pywikibot
-    try:
-        return
-        from bot import Bot
-        bot = Bot('lol')
-        bot.run()
+    global config_file
+    if checkUserConfig():
+        import pywikibot
+        from bot import bot
         
-    except (pywikibot.bot.QuitKeyboardInterrupt, KeyboardInterrupt) as e:
-        pywikibot.output('\r\n\03{lightaqua}Stopping open threads\03{default} - to force quit press Ctrl+C' + ' again' if type(e) is KeyboardInterrupt else '')
         try:
-            pywikibot.stopme()
-        except KeyboardInterrupt:
-            pass
+            bot().run()
+        except (pywikibot.bot.QuitKeyboardInterrupt, KeyboardInterrupt) as e:
+            pywikibot.output('\r\n\03{lightaqua}Stopping open threads\03{default} - to force quit press Ctrl+C' + ' again' if type(e) is KeyboardInterrupt else '')
+            try:
+                pywikibot.stopme()
+            except KeyboardInterrupt:
+                pass
+    else:
+        from pywikibot import output
+        if os.path.exists(config_file):
+            output('\r\n\03{lightgreen}Config file is in place, but you have to run the script again\03{default}')
+        else:
+            output('\r\n\03{lightred}Config file creation failed - try again\03{default}')
+        
 
 if __name__ == '__main__':
     main()
